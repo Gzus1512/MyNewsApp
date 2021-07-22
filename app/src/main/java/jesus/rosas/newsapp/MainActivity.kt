d82d48190687d97942589eab99346ee1bd171f51
Gzus1512 <86988371+Gzus1512@users.noreply.github.com>
2021-07-21 20:38:21 -0500

    Splash

--- app/src/main/java/jesus/rosas/newsapp/MainActivity.kt
+++ app/src/main/java/jesus/rosas/newsapp/MainActivity.kt
@@ -11,8 +11,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.Theme_NewsApp)
-
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
